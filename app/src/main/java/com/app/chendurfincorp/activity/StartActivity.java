920cdccf1f5a10357b77725d69ab890bc27663e9
vinothrm1990 <droidcodermyself@gmail.com>
2018-08-30 17:41:38 +0530

    Initial commit

---
+++ app/src/main/java/com/app/chendurfincorp/activity/StartActivity.java
@@ -0,0 +1,15 @@
+package com.app.chendurfincorp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.app.chendurfincorp.R;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}

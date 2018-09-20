832da28c20b6ed9739ac45a5d8333faecd2f051d
vinothrm1990 <droidcodermyself@gmail.com>
2018-09-20 11:03:15 +0530

    Updated(20.9.18)

--- app/src/main/java/com/app/chendurfincorp/helper/ChendurFincorp.java
+++ app/src/main/java/com/app/chendurfincorp/helper/ChendurFincorp.java
@@ -1,15 +1,29 @@
 package com.app.chendurfincorp.helper;
 
 import android.app.Application;
+import android.content.Context;
+
 import com.treebo.internetavailabilitychecker.InternetAvailabilityChecker;
 
+import org.acra.ACRA;
+import org.acra.annotation.ReportsCrashes;
+
+@ReportsCrashes(mailTo = "shadowwsvinothkumar@gmail.com")
 public class ChendurFincorp extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
 
+        ACRA.init(this);
         InternetAvailabilityChecker.init(this);
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+
+        ACRA.init(this);
     }
 
     @Override
